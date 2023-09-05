d33ad65b9f7c66456b9c2a9a758cae19844d5fb4
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-05 12:56:21 -0500

    Replace 'type' class variable to 'classification' for more clarity

--- AP_Randomizer/apworld/Items.py
+++ AP_Randomizer/apworld/Items.py
@@ -8,7 +8,7 @@ class PseudoregaliaItem(Item):
 
 class PseudoregaliaItemData(NamedTuple):
     code: Optional[int] = None
-    type: ItemClassification = ItemClassification.filler
+    classification: ItemClassification = ItemClassification.filler
     can_create: Callable[[MultiWorld, int], bool] = lambda multiworld, player: True
 
 
@@ -16,67 +16,67 @@ class PseudoregaliaItemData(NamedTuple):
 item_data_table: Dict[str, PseudoregaliaItemData] = {
     "Dream Breaker": PseudoregaliaItemData(
         code=2365810001,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Indignation": PseudoregaliaItemData(
         code=2365810002,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Sun Greaves": PseudoregaliaItemData(
         code=2365810003,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Slide": PseudoregaliaItemData(
         code=2365810004,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Solar Wind": PseudoregaliaItemData(
         code=2365810005,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Sunsetter": PseudoregaliaItemData(
         code=2365810006,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Strikebreak": PseudoregaliaItemData(
         code=2365810007,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Cling Gem": PseudoregaliaItemData(
         code=2365810008,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Ascendant Light": PseudoregaliaItemData(
         code=2365810009,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     """
     "Clear Mind": PseudoregaliaItemData(
         code=2365810011,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Empathy": PseudoregaliaItemData(
         code=2365810012,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Good Graces": PseudoregaliaItemData(
         code=2365810013,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Martial Prowess": PseudoregaliaItemData(
         code=2365810014,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Pilgrimage": PseudoregaliaItemData(
         code=2365810015,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Aerial Finesse": PseudoregaliaItemData(
         code=2365810016,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Heliacal Power": PseudoregaliaItemData(
         code=2365810017,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
     "Professionalism": PseudoregaliaItemData(
         code=2365810018,
-        type=ItemClassification.filler),
+        classification=ItemClassification.filler),
     "Health Piece": PseudoregaliaItemData(
         code=2365810019,
-        type=ItemClassification.useful),
+        classification=ItemClassification.useful),
     "Small Key": PseudoregaliaItemData(
         code=2365810020,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
         """
     # Moved soul cutter down here temporarily because otherwise the block comment throws an error
     "Soul Cutter": PseudoregaliaItemData(
         code=2365810010,
-        type=ItemClassification.progression),
+        classification=ItemClassification.progression),
 }
 
 item_table = {name: data.code for name, data in item_data_table.items() if data.code is not None}
