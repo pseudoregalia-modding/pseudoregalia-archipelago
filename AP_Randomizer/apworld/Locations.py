8efcd2ca8df0c6f8dfa11616a39aef1eb035d893
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-05 12:32:39 -0500

    Set address and region data for abilities in Location.py

--- AP_Randomizer/apworld/Locations.py
+++ AP_Randomizer/apworld/Locations.py
@@ -1,8 +1,12 @@
 from BaseClasses import Location, MultiWorld
 from typing import Callable, Dict, NamedTuple, Optional
+from enum import Enum
+from .Regions import ZONE
+
 
 class PseudoregaliaLocation(Location):
     game = "Pseudoregalia"
+
 
 class PseudoregaliaLocationData(NamedTuple):
     region: str
@@ -10,18 +14,41 @@ class PseudoregaliaLocationData(NamedTuple):
     can_create: Callable[[MultiWorld, int], bool] = lambda multiworld, player: True
     locked_item: Optional[str] = None
 
+
 location_data_table = {
-    "The Beginning": PseudoregaliaLocationData(
-        address=4206942067,
-        region="The Dream"),
-    "The Middle": PseudoregaliaLocationData(
-        address=4206942068,
-        region="The Dream"),
-    "The End": PseudoregaliaLocationData(
-        address=4206942069,
-        region="The Dream",
-        locked_item="Dream Maker"),
+    ZONE.DUNGEON + " - Dream Breaker": PseudoregaliaLocationData(
+        # Dream Breaker can't be shuffled right now but I would like to later
+        address=2365810001,
+        region=ZONE.DUNGEON),
+    ZONE.DUNGEON + " - Slide": PseudoregaliaLocationData(
+        address=2365810002,
+        region=ZONE.DUNGEON),
+    ZONE.CASTLE + " - Indignation": PseudoregaliaLocationData(
+        address=2365810003,
+        region=ZONE.CASTLE),
+    ZONE.LIBRARY + " - Sun Greaves": PseudoregaliaLocationData(
+        address=2365810004,
+        region=ZONE.LIBRARY),
+    ZONE.KEEP + " - Sunsetter": PseudoregaliaLocationData(
+        address=2365810005,
+        region=ZONE.KEEP),
+    ZONE.KEEP + " - Strikebreak": PseudoregaliaLocationData(
+        address=2365810006,
+        region=ZONE.KEEP),
+    ZONE.THEATRE + " - Soul Cutter": PseudoregaliaLocationData(
+        address=2365810007,
+        region=ZONE.THEATRE),
+    ZONE.UNDERBELLY + " - Ascendant Light": PseudoregaliaLocationData(
+        address=2365810008,
+        region=ZONE.UNDERBELLY),
+    ZONE.BAILEY + " - Solar Wind": PseudoregaliaLocationData(
+        address=2365810009,
+        region=ZONE.BAILEY),
+    ZONE.TOWER + " - Cling Gem": PseudoregaliaLocationData(
+        address=2365810010,
+        region=ZONE.TOWER),
 }
 
 location_table = {name: data.address for name, data in location_data_table.items() if data.address is not None}
 # locked_locations = {name: data for name, data in location_data_table.items() if data.locked_item}
+77
