9ce3bc1b0f8c00d3b4831e6646756b2c0a3bad6b
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-05 20:06:17 -0500

    Create the MapManager class

---
+++ AP_Randomizer/include/MapManager.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "APCollectible.hpp"
+
+using namespace RC;
+using namespace RC::Unreal;
+
+namespace Pseudoregalia_AP {
+	class MapManager {
+	public:
+		MapManager();
+		void SpawnActors(std::vector<APCollectible>);
+
+	private:
+
+	};
+
+}
