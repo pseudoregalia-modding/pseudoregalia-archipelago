fd1d1ba786b1d46500f47cb5a423b484db712caf
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-07 15:12:48 -0500

    Add line breaks to zone_table to improve readability

--- AP_Randomizer/src/APClient.cpp
+++ AP_Randomizer/src/APClient.cpp
@@ -32,14 +32,32 @@ namespace Pseudoregalia_AP {
 
     void APClient::FillTable() {
         this->zone_table = {
-            { L"ZONE_Dungeon", std::vector<APCollectible>{APCollectible("Dungeon", FVector(-3500.0, 4950.0, -50.0), 2365810001), APCollectible("Dungeon", FVector(16650, 2600, 2350), 2365810002)}},
-            { L"ZONE_LowerCastle", std::vector<APCollectible>{APCollectible("Castle", FVector(5400, 2100, -550), 2365810003)} },
-            { L"Zone_Upper", std::vector<APCollectible>{APCollectible("Dungeon", FVector(-3000, 4900, -400), 2365810004), APCollectible("Dungeon", FVector(10050, 1800, 85), 2365810005)} },
-            { L"Zone_Library", std::vector<APCollectible>{APCollectible("Library", FVector(-4150, 9200, -100), 2365810006)} },
-            { L"Zone_Theatre", std::vector<APCollectible>{APCollectible("Theatre", FVector(8500, 7850, -1400), 2365810007)} },
-            { L"ZONE_Exterior", std::vector<APCollectible>{APCollectible("Bailey", FVector(-1100, 10850, 150), 2365810008)} },
-            { L"Zone_Caves", std::vector<APCollectible>{APCollectible("Underbelly", FVector(-5400, 6650, 6750), 2365810009)} },
-            { L"Zone_Tower", std::vector<APCollectible>{APCollectible("Tower", FVector(13350, 5250, 4150), 2365810010)} },
+            {L"ZONE_Dungeon", std::vector<APCollectible> {
+                APCollectible("Dungeon", FVector(-3500.0, 4950.0, -50.0), 2365810001),
+                APCollectible("Dungeon", FVector(16650, 2600, 2350), 2365810002)}
+                },
+            {L"ZONE_LowerCastle", std::vector<APCollectible> {
+                APCollectible("Castle", FVector(5400, 2100, -550), 2365810003)}
+                },
+            {L"Zone_Upper", std::vector<APCollectible>{
+                APCollectible("Dungeon", FVector(-3000, 4900, -400), 2365810004),
+                APCollectible("Dungeon", FVector(10050, 1800, 85), 2365810005)}
+                },
+            {L"Zone_Library", std::vector<APCollectible>{
+                APCollectible("Library", FVector(-4150, 9200, -100), 2365810006)}
+                },
+            {L"Zone_Theatre", std::vector<APCollectible>{
+                APCollectible("Theatre", FVector(8500, 7850, -1400), 2365810007)}
+                },
+            {L"ZONE_Exterior", std::vector<APCollectible>{
+                APCollectible("Bailey", FVector(-1100, 10850, 150), 2365810008)}
+                },
+            {L"Zone_Caves", std::vector<APCollectible>{
+                APCollectible("Underbelly", FVector(-5400, 6650, 6750), 2365810009)
+                }},
+            {L"Zone_Tower", std::vector<APCollectible>{
+                APCollectible("Tower", FVector(13350, 5250, 4150), 2365810010)}
+                },
         };
     }
 
