74c366a55c674ca69d51538666de5ea4668bc78b
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-08 16:42:03 -0500

    Change FillZoneTable(), ResetUpgradeTable(), ClearItems(), ReceiveItems(), and CheckLocation() to be private

--- AP_Randomizer/include/APClient.hpp
+++ AP_Randomizer/include/APClient.hpp
@@ -8,15 +8,15 @@ namespace Pseudoregalia_AP {
 	class APClient {
 	public:
 		static void Initialize();
+		static void Connect(const char*, const char*, const char*);
+		static void SendCheck(int64_t, std::wstring);
+		static void OnMapLoad(AActor*, UFunction*, std::wstring);
+	private:
 		static void FillZoneTable();
 		static void ResetUpgradeTable();
 		static void ClearItems();
 		static void ReceiveItem(int64_t, bool);
 		static void CheckLocation(int64_t);
-		static void Connect(const char*, const char*, const char*);
-		static void SendCheck(int64_t, std::wstring);
-		static void OnMapLoad(AActor*, UFunction*, std::wstring);
-	private:
 		static std::map<int64_t, std::string> lookup_id_to_item;
 		static std::map <std::wstring, std::vector<APCollectible>> zone_table;
 		static std::map<std::string, int> upgrade_table;
