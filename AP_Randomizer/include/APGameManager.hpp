9154f215ff6006b0ec1e89d33591c90cc111530a
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-09 16:51:12 -0500

    Add APGameManager files to delegate responsibilities for communicating with UE objects directly

---
+++ AP_Randomizer/include/APGameManager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <DynamicOutput/DynamicOutput.hpp>
+#include "APCollectible.hpp"
+
+using namespace RC;
+using namespace RC::Unreal;
+
+namespace Pseudoregalia_AP {
+	class APGameManager {
+	public:
+	private:
+	};
+}
---
+++ AP_Randomizer/src/APGameManager.cpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "APGameManager.hpp"
+
+namespace Pseudoregalia_AP {
+
+}
