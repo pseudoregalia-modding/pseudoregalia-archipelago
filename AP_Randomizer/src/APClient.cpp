b47c395cb7ec44fad7fa6467e1647045a1c273ca
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-09 13:31:19 -0500

    Remove two unnecessary lines from SyncItems() by more directly passing parameters to the AddUpgradeInfo struct

--- AP_Randomizer/src/APClient.cpp
+++ AP_Randomizer/src/APClient.cpp
@@ -155,15 +155,13 @@ namespace Pseudoregalia_AP {
 
         for (auto const& pair : upgrade_table)
         {
-            FName* name_ptr = new FName(pair.first);
-            FName new_name = *name_ptr;
-            int new_count = pair.second;
+            FName new_name = *new FName(pair.first);
 
             AddUpgradeInfo params = {
                 new_name,
-                new_count,
+                pair.second,
             };
-            Output::send<LogLevel::Verbose>(STR("Attempting to add {} with value {}..."), pair.first, new_count);
+            Output::send<LogLevel::Verbose>(STR("Attempting to add {} with value {}..."), pair.first, pair.second);
 
             randomizer_blueprint->ProcessEvent(add_upgrade_function, &params);
         }
