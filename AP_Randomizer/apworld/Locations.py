09b68bc4706e757945262936433455ff5af1713c
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 15:24:38 -0500

    Create location_table dictionary with major upgrade locations

--- AP_Randomizer/apworld/Locations.py
+++ AP_Randomizer/apworld/Locations.py
@@ -4,3 +4,33 @@ from typing import NamedTuple, Dict
 
 class PseudoregaliaLocation(Location):
     game: str = "Pseudoregalia"
+
+
+class PseudoregaliaLocationData(NamedTuple):
+    region: str
+    code: int = None
+
+
+location_table = {
+    "Dungeon - Dream Breaker": PseudoregaliaLocationData(
+        # Dream Breaker can't really be shuffled right now but I would like to later
+        code=2365810001,),
+    "Dungeon - Slide": PseudoregaliaLocationData(
+        code=2365810002,),
+    "Castle - Indignation": PseudoregaliaLocationData(
+        code=2365810003,),
+    "Keep - Sunsetter": PseudoregaliaLocationData(
+        code=2365810004,),
+    "Keep - Strikebreak": PseudoregaliaLocationData(
+        code=2365810005,),
+    "Library - Sun Greaves": PseudoregaliaLocationData(
+        code=2365810006,),
+    "Theatre - Soul Cutter": PseudoregaliaLocationData(
+        code=2365810007,),
+    "Bailey - Solar Wind": PseudoregaliaLocationData(
+        code=2365810008,),
+    "Underbelly - Ascendant Light": PseudoregaliaLocationData(
+        code=2365810009,),
+    "Tower - Cling Gem": PseudoregaliaLocationData(
+        code=2365810010,),
+}
