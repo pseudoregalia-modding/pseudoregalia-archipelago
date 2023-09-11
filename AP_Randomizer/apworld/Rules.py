edf36ed60e949e50997ba45a009789fb00cadacb
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 14:48:48 -0500

    Reset apworld files completely to write from scratch

--- AP_Randomizer/apworld/Rules.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable
-
-from BaseClasses import CollectionState, MultiWorld
-
-
-def get_encouragement_rule(multiworld: MultiWorld, player:int) -> Callable[[CollectionState], bool]:
-    return lambda state: state.has("Dream Maker", player)
