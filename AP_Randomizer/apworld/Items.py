278e59b335a1816152b5ee80dd26a353d64ca5d3
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 14:56:20 -0500

    Move item data into separate PseudoregaliaItemData class for easier use with dictionaries

--- AP_Randomizer/apworld/Items.py
+++ AP_Randomizer/apworld/Items.py
@@ -1,7 +1,11 @@
 from BaseClasses import Item, ItemClassification
+from typing import NamedTuple
 
 
 class PseudoregaliaItem(Item):
     game = "Pseudoregalia"
+
+
+class PseudoregaliaItemData(NamedTuple):
     code: int = None
     classification: ItemClassification = ItemClassification.filler
