c4f55c7b8ca7fa714f6d27dbe448971f402504b5
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 18:19:56 -0500

    Create baseline region_table

--- AP_Randomizer/apworld/Regions.py
+++ AP_Randomizer/apworld/Regions.py
@@ -5,3 +5,8 @@ from BaseClasses import CollectionState
 class RegionExit(NamedTuple):
     region: str
     access_rule: Callable[[CollectionState, int], bool] = lambda state, player: True
+
+
+region_table: Dict[str, List[RegionExit]] = {
+    "Menu": [RegionExit("The Mirror")],
+}
