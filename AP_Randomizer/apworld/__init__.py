cd16800a193d87fb71c2504a913bb622e2dffe4d
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 15:01:04 -0500

    Create create_item() in init

--- AP_Randomizer/apworld/__init__.py
+++ AP_Randomizer/apworld/__init__.py
@@ -1,5 +1,10 @@
 from worlds.AutoWorld import World
+from .Items import PseudoregaliaItem, PseudoregaliaItemData, item_table
 
 
 class PseudoregaliaWorld(World):
     game = "Pseudoregalia"
+
+    def create_item(self, name: str) -> PseudoregaliaItem:
+        data = item_table[name]
+        return PseudoregaliaItem(name, data.classification, data.code, self.player)
