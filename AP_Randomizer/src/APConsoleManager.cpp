6641f633cb98d5cc54d351c21132b20ad396f960
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 13:05:40 -0500

    Populate ParseConnect() body to send connection info to APClient::Connect()

--- AP_Randomizer/src/APConsoleManager.cpp
+++ AP_Randomizer/src/APConsoleManager.cpp
@@ -35,11 +35,34 @@ namespace Pseudoregalia_AP {
 	}
 
 	void APConsoleManager::ParseConnect(std::string args) {
+		while (args[0] == DELIM) {
+			args.erase(args.begin());
+		}
 		if (args.empty()) {
-			std::cout << "Please provide an address.\n";
+			std::cout << "Please provide an ip address, slot name, and (if necessary) password.\n";
 			return;
 		}
-
 		std::string ip = args.substr(0, args.find(DELIM));
+		args.erase(0, args.find(DELIM));
+		std::cout << "ip:" << ip << "\n";
+
+		while (args[0] == DELIM) {
+			args.erase(args.begin());
+		}
+		if (args.empty()) {
+			std::cout << "Please provide a slot name and (if necessary) password.\n";
+			return;
+		}
+		std::string slot_name = args.substr(0, args.find(DELIM));
+		args.erase(0, args.find(DELIM));
+		std::cout << "slot name:" << slot_name << "\n";
+		
+		while (args[0] == DELIM) {
+			args.erase(args.begin());
+		}
+		std::string password = args.substr(0, args.find(DELIM));
+		std::cout << "password:" << password << "\n";
+
+		APClient::Connect(ip.c_str(), slot_name.c_str(), password.c_str());
 	}
 }
