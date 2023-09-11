2185272f0250147378746aaf816868e06f47c7cb
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 11:20:05 -0500

    Add ConsoleCommand body to identify a connect command

--- AP_Randomizer/src/APConsoleManager.cpp
+++ AP_Randomizer/src/APConsoleManager.cpp
@@ -4,6 +4,18 @@
 namespace Pseudoregalia_AP {
 	void APConsoleManager::ProcessCommand(const Unreal::TCHAR* new_command) {
 		std::string command = ConvertTcharToString(new_command);
+		std::string delimiter = " ";
+		std::string first_word = command.substr(0, command.find(delimiter));
+
+		transform(
+			first_word.begin(), first_word.end(),
+			first_word.begin(),
+			tolower);
+
+		if (first_word == "connect") {
+			command.erase(0, command.find(delimiter) + delimiter.length());
+			// APConsoleManager::ParseConnect(command);
+		}
 	}
 
 	std::string APConsoleManager::ConvertTcharToString(const Unreal::TCHAR* tchars) {
