d00319026b99b387e223f4b34436a198d9550715
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 18:15:19 -0500

    Add example for complex logic rule in set_rules()

--- AP_Randomizer/apworld/__init__.py
+++ AP_Randomizer/apworld/__init__.py
@@ -1,5 +1,7 @@
 from worlds.AutoWorld import World
 from .Items import PseudoregaliaItem, PseudoregaliaItemData, item_table
+from worlds.generic.Rules import add_rule, set_rule, forbid_item
+from .Rules import get_kicks, can_bounce
 
 
 class PseudoregaliaWorld(World):
@@ -8,3 +10,12 @@ class PseudoregaliaWorld(World):
     def create_item(self, name: str) -> PseudoregaliaItem:
         data = item_table[name]
         return PseudoregaliaItem(name, data.classification, data.code, self.player)
+
+    def set_rules(self):
+        set_rule(self.multiworld.get_location("Dungeon - HP By Breaker", self.player),
+                 lambda state:
+                 any([
+                     can_bounce(state, self.player),
+                     get_kicks(state, self.player) == 3,
+                     state.has("Sunsetter", self.player)
+                 ]))
