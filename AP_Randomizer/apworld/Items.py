7abd4147b2392e422f0c84da3f201de9135c3c01
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-11 14:58:20 -0500

    Create item_table dictionary with major upgrades

--- AP_Randomizer/apworld/Items.py
+++ AP_Randomizer/apworld/Items.py
@@ -1,5 +1,5 @@
 from BaseClasses import Item, ItemClassification
-from typing import NamedTuple
+from typing import NamedTuple, Dict
 
 
 class PseudoregaliaItem(Item):
@@ -9,3 +9,37 @@ class PseudoregaliaItem(Item):
 class PseudoregaliaItemData(NamedTuple):
     code: int = None
     classification: ItemClassification = ItemClassification.filler
+
+
+item_table: Dict[str, PseudoregaliaItemData] = {
+    "Dream Breaker": PseudoregaliaItemData(
+        code=2365810001,
+        classification=ItemClassification.progression),
+    "Indignation": PseudoregaliaItemData(
+        code=2365810002,
+        classification=ItemClassification.useful),
+    "Sun Greaves": PseudoregaliaItemData(
+        code=2365810003,
+        classification=ItemClassification.progression),
+    "Slide": PseudoregaliaItemData(
+        code=2365810004,
+        classification=ItemClassification.progression),
+    "Solar Wind": PseudoregaliaItemData(
+        code=2365810005,
+        classification=ItemClassification.progression),
+    "Sunsetter": PseudoregaliaItemData(
+        code=2365810006,
+        classification=ItemClassification.progression),
+    "Strikebreak": PseudoregaliaItemData(
+        code=2365810007,
+        classification=ItemClassification.progression),
+    "Cling Gem": PseudoregaliaItemData(
+        code=2365810008,
+        classification=ItemClassification.progression),
+    "Ascendant Light": PseudoregaliaItemData(
+        code=2365810009,
+        classification=ItemClassification.progression),
+    "Soul Cutter": PseudoregaliaItemData(
+        code=2365810010,
+        classification=ItemClassification.progression),
+}
