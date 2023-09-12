507ff79ea301171969d089cdfe164132a599448b
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-12 17:08:37 -0500

    Begin populating region_table with access rules for dungeon regions

--- AP_Randomizer/apworld/Regions.py
+++ AP_Randomizer/apworld/Regions.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple, Callable, Dict, List
 from BaseClasses import CollectionState
+from Rules import has_breaker, can_bounce, get_kicks, has_small_keys, navigate_darkrooms
 
 
 class RegionExit(NamedTuple):
@@ -9,4 +10,25 @@ class RegionExit(NamedTuple):
 
 region_table: Dict[str, List[RegionExit]] = {
     "Menu": [RegionExit("The Mirror")],
+    "The Mirror": [RegionExit("Dungeon: First Save Point", has_breaker)],
+    "Dungeon First Save Point": [RegionExit("The Mirror", has_breaker),
+                                 RegionExit("Dungeon By Slide", has_breaker)],
+    "Dungeon By Slide": [RegionExit("Dungeon First Save Point", has_breaker),
+                         RegionExit("Dungeon Near Strong Eyes",
+                                    lambda state, player: state.has(state, "Slide", player)),
+                         RegionExit("Dungeon Escape Lower", has_breaker)],
+    "Dungeon Near Strong Eyes": [RegionExit("Dungeon Near Slide",
+                                            lambda state, player: state.has(state, "Slide", player)),
+                                 RegionExit("Dungeon By Castle", has_small_keys)],
+    "Dungeon By Castle": [RegionExit("The Mirror"),
+                          RegionExit("Castle Halls")],
+    "Dungeon Escape Lower": [RegionExit("Dungeon By Slide", has_breaker),
+                             RegionExit("Dungeon Escape Upper",
+                                        lambda state, player:
+                                        any(
+                                            can_bounce(state, player),
+                                            state.has(state, "Sunsetter", player) and get_kicks == 2,
+                                            # state.has(state, "Cling Gem", player)
+                                        ))],
+    "Dungeon Escape Upper": [RegionExit("Dungeon Escape Lower")],
 }
