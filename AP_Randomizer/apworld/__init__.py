fdd5f06c0a02d674a9ea90b69834b66b0a55de0c
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-14 17:59:54 -0500

    Add item_name_to_id and location_name_to_id members

--- AP_Randomizer/apworld/__init__.py
+++ AP_Randomizer/apworld/__init__.py
@@ -1,11 +1,15 @@
 from worlds.AutoWorld import World
 from .Items import PseudoregaliaItem, PseudoregaliaItemData, item_table
+from .Locations import location_table
 from worlds.generic.Rules import add_rule, set_rule, forbid_item
 from .Rules import get_kicks, can_bounce
 
 
 class PseudoregaliaWorld(World):
     game = "Pseudoregalia"
+
+    item_name_to_id = {name: data.code for name, data in item_table.items()}
+    location_name_to_id = {name: data.code for name, data in location_table.items()}
 
     def create_item(self, name: str) -> PseudoregaliaItem:
         data = item_table[name]
