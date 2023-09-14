9131602672ed5389246cc960b4b191671a3e4faa
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-14 18:10:40 -0500

    Add create_regions() method to connect Regions and append them to the multiworld

--- AP_Randomizer/apworld/__init__.py
+++ AP_Randomizer/apworld/__init__.py
@@ -1,6 +1,8 @@
 from worlds.AutoWorld import World
+from BaseClasses import Region
 from .Items import PseudoregaliaItem, PseudoregaliaItemData, item_table
 from .Locations import location_table
+from .Regions import region_table
 from worlds.generic.Rules import add_rule, set_rule, forbid_item
 from .Rules import get_kicks, can_bounce
 
@@ -14,3 +16,16 @@ class PseudoregaliaWorld(World):
     def create_item(self, name: str) -> PseudoregaliaItem:
         data = item_table[name]
         return PseudoregaliaItem(name, data.classification, data.code, self.player)
+
+    def create_regions(self):
+        for region_name in region_table.keys():
+            self.multiworld.regions.append(Region(region_name, self.player, self.multiworld))
+
+        # TODO: assign locations
+
+        for region_name, exit_list in region_table.items():
+            region = self.multiworld.get_region(region_name, self.player)
+
+            for exit in exit_list:
+                connecting_region = self.multiworld.get_region(exit.region, self.player)
+                region.connect(connecting_region, None, exit.access_rule if exit.access_rule else None)
