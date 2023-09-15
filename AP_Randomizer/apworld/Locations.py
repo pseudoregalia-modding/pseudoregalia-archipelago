e5e7aac113119346a61d44452deb56a1d44a1ee9
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-15 18:26:26 -0500

    Add new regions to location_table

--- AP_Randomizer/apworld/Locations.py
+++ AP_Randomizer/apworld/Locations.py
@@ -11,25 +11,32 @@ location_table = {
     "Dungeon - Dream Breaker": PseudoregaliaLocationData(
         # Dream Breaker can't really be shuffled right now but I would like to later
         code=2365810001,
-        region="The Mirror"),
+        region="Dungeon Mirror"),
     "Dungeon - Slide": PseudoregaliaLocationData(
         code=2365810002,
-        region="Dungeon By Slide"),
+        region="Dungeon Mirror"),
     "Castle - Indignation": PseudoregaliaLocationData(
         code=2365810003,
-        region="Castle Halls"),
+        region="Castle Sansa"),
     "Keep - Sunsetter": PseudoregaliaLocationData(
-        code=2365810004,),
+        code=2365810004,
+        region="Keep Sunsetter"),
     "Keep - Strikebreak": PseudoregaliaLocationData(
-        code=2365810005,),
+        code=2365810005,
+        region="Keep Main"),
     "Library - Sun Greaves": PseudoregaliaLocationData(
-        code=2365810006,),
+        code=2365810006,
+        region="Library Main"),
     "Theatre - Soul Cutter": PseudoregaliaLocationData(
-        code=2365810007,),
+        code=2365810007,
+        region="Theatre Main"),
     "Bailey - Solar Wind": PseudoregaliaLocationData(
-        code=2365810008,),
+        code=2365810008,
+        region="Empty Bailey"),
     "Underbelly - Ascendant Light": PseudoregaliaLocationData(
-        code=2365810009,),
+        code=2365810009,
+        region="Underbelly Main"),
     "Tower - Cling Gem": PseudoregaliaLocationData(
-        code=2365810010,),
+        code=2365810010,
+        region="Tower Remains"),
 }
