22d688c38d30c92bd67d4416662eadfb24f65445
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-15 20:01:51 -0500

    Fix broken calls to any()

--- AP_Randomizer/apworld/Regions.py
+++ AP_Randomizer/apworld/Regions.py
@@ -17,12 +17,12 @@ region_table: Dict[str, List[RegionExit]] = {
                        RegionExit("Underbelly Main", has_breaker),
                        RegionExit("Theatre Main",
                                   lambda state, player:
-                                  any(
+                                  any([
                                       get_kicks(state, player) >= 3 and has_breaker,
                                       state.has("Cling Gem", player) and has_breaker,
                                       can_slidejump(state, player) and get_kicks(state, player) >= 1 and has_breaker,
                                       can_bounce(state, player),
-                                  )
+                                  ])
                                   )],
     "Dungeon Strong Eyes": [RegionExit("Castle Sansa", has_small_keys)],
     "Castle Sansa": [RegionExit("Library Main", has_breaker),
@@ -30,47 +30,47 @@ region_table: Dict[str, List[RegionExit]] = {
                      RegionExit("Empty Bailey"),
                      RegionExit("Theatre Pillar",
                                 lambda state, player:
-                                any(
+                                any([
                                     get_kicks(state, player) > 0,
                                     state.has("Cling Gem", player),
                                     state.has("Sunsetter", player),
-                                )),
+                                ])),
                      RegionExit("Theatre Main",
                                 lambda state, player:
-                                any(
+                                any([
                                     state.has("Cling Gem", player),
                                     can_slidejump(state, player) and get_kicks(state, player) >= 4,
-                                ))],
+                                ]))],
     "Library Main": [RegionExit("Library Locked", has_small_keys)],
     "Library Locked": [],  # There's no point in connecting this back to library main.
     "Keep Main": [RegionExit("Keep Sunsetter",
                              lambda state, player:
-                             any(
+                             any([
                                  has_small_keys(state, player),
                                  state.has("Cling Gem", player),
                                  get_kicks(state, player) >= 3,
-                             )),
+                             ])),
                   RegionExit("Underbelly Hole",
                              lambda state, player:
-                             any(
+                             any([
                                  get_kicks(state, player) > 0,
                                  state.has("Sunsetter", player),
-                             )),
+                             ])),
                   RegionExit("Theatre Main",
                              lambda state, player:
-                             any(
+                             any([
                                  state.has("Cling Gem", player) and get_kicks(state, player) >= 3,
                                  state.has("Cling Gem", player) and can_slidejump(state, player),
-                             ))],
+                             ]))],
     "Keep Sunsetter": [],
     "Empty Bailey": [RegionExit("Castle Sansa"),
                      RegionExit("Tower Remains",
                                 lambda state, player:
-                                any(
+                                any([
                                     get_kicks(state, player) > 0,
                                     state.has("Cling Gem", player),
                                     state.has_all(["Slide", "Sunsetter"], player)
-                                )),
+                                ])),
                      RegionExit("Theatre Pillar")],
     "Tower Remains": [RegionExit("Underbelly Main",  # Simplified access rule copied from spuds' logic.
                                  lambda state, player: state.has("Sunsetter", player)),
@@ -79,19 +79,19 @@ region_table: Dict[str, List[RegionExit]] = {
                                  state.has("Cling Gem", player) and get_kicks(state, player) >= 3)],
     "Underbelly Main": [RegionExit("Empty Bailey",
                                    lambda state, player:
-                                   any(
+                                   any([
                                        has_breaker(state, player),
                                        state.has("Sunsetter", player),
-                                   ))],
+                                   ]))],
     "Underbelly Hole": [RegionExit("Underbelly Main",
                                    lambda state, player: state.has("Sunsetter", player))],  # I don't actually know what this is.
     "Theatre Main": [RegionExit("Keep Main",
                                 lambda state, player: state.has("Cling Gem", player))],
     "Theatre Pillar": [RegionExit("Theatre Main",
                                   lambda state, player:
-                                  any(
+                                  any([
                                       state.has_all(["Sunsetter", "Cling Gem"], player),
                                       state.has("Sunsetter", player) and get_kicks(state, player) >= 4,
-                                  ))],
+                                  ]))],
     "The Great Door": [],
 }
--- AP_Randomizer/apworld/Rules.py
+++ AP_Randomizer/apworld/Rules.py
@@ -45,54 +45,54 @@ location_rules: Dict[str, Callable[[CollectionState, int], bool]] = {
     "Keep - Sunsetter": has_breaker,
     "Keep - Strikebreak": lambda state, player:
         (state.has("Slide", player) or can_strikebreak(state, player))
-        and any(
+        and any([
             can_slidejump(state, player),
             get_kicks(state, player) >= 1,
             state.has("Cling Gem", player),
-        ),
+        ]),
     "Library - Sun Greaves": lambda state, player:
-        any(
+        any([
             state.has("Slide", player) and has_breaker(state, player),
             state.has("Cling Gem", player) and has_breaker(state, player),
             get_kicks(state, player) >= 4,
             can_slidejump(state, player),
-        ),
+        ]),
     "Theatre - Soul Cutter": can_strikebreak,
     "Bailey - Solar Wind": lambda state, player: state.has("Slide", player),
     "Underbelly - Ascendant Light": lambda state, player:
-        any(
+        any([
             can_bounce(state, player),
             get_kicks(state, player) >= 3,
             state.has("Cling Gem", player),
             can_slidejump(state, player) and get_kicks(state, player) > 0,
-            ),
+        ]),
     "Tower - Cling Gem": lambda state, player: get_kicks(state, player) >= 3,
 
     "Bailey - Major Key": lambda state, player:
-        any(
+        any([
             state.has("Sunsetter", player),
             state.has("Cling Gem", player),
             get_kicks(state, player) >= 3,
-    ),
+        ]),
     "Underbelly - Major Key": lambda state, player:
         state.has("Sunsetter", player)
-        and any(
+        and any([
             can_soulcutter(state, player) and can_bounce(state, player),
             can_slidejump and get_kicks >= 3,
             can_soulcutter(state, player) and state.has("Cling Gem"),
-    ),
+        ]),
     "Tower - Major Key": lambda state, player:
         state.has("Cling Gem", player) and get_kicks >= 3,
     "Theatre - Major Key": lambda state, player:
         can_soulcutter(state, player)
-        and any(
+        and any([
             state.has("Cling Gem", player) and can_slidejump,
             state.has("Cling Gem", player) and get_kicks > 0,
-    ),
+        ]),
     "Keep - Major Key": lambda state, player:
-        any(
+        any([
             state.has_all(["Cling Gem", "Sunsetter"], player) and can_bounce(state, player),
             state.has("Cling Gem", player) and can_bounce(state, player) and get_kicks(state, player) >= 3,
             state.has_all(["Sunsetter", "Cling Gem"], player) and can_bounce(state, player) and get_kicks >= 3,
-    )
+        ])
 }
