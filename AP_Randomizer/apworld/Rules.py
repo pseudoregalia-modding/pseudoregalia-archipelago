a030e4d1e55f273c560bcd8a3cba2f8305626f8f
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-15 19:06:30 -0500

    Create can_strikebreak and can_soulcutter functions, and replace state.has_all calls with them

--- AP_Randomizer/apworld/Rules.py
+++ AP_Randomizer/apworld/Rules.py
@@ -32,11 +32,19 @@ def can_slidejump(state: CollectionState, player: int) -> bool:
     return (state.has_all(state, ["Slide", "Solar Wind"]))
 
 
+def can_strikebreak(state: CollectionState, player: int) -> bool:
+    return (state.has_all(["Dream Breaker", "Strikebreak"]))
+
+
+def can_soulcutter(state: CollectionState, player: int) -> bool:
+    return (state.has_all(["Dream Breaker", "Strikebreak", "Soul Cutter"]))
+
+
 location_rules: Dict(str, Callable[[CollectionState, int], bool]) = {
     "Dungeon - Slide": has_breaker,
     "Keep - Sunsetter": has_breaker,
     "Keep - Strikebreak": lambda state, player:
-        (state.has("Slide", player) or state.has("Strikebreak", player))
+        (state.has("Slide", player) or can_strikebreak(state, player))
         and any(
             can_slidejump(state, player),
             get_kicks(state, player) >= 1,
@@ -49,7 +57,7 @@ location_rules: Dict(str, Callable[[CollectionState, int], bool]) = {
             get_kicks(state, player) >= 4,
             can_slidejump(state, player),
         ),
-    "Theatre - Soul Cutter": lambda state, player: state.has("Strikebreak", player),
+    "Theatre - Soul Cutter": can_strikebreak,
     "Bailey - Solar Wind": lambda state, player: state.has("Slide", player),
     "Underbelly - Ascendant Light": lambda state, player:
         any(
@@ -57,7 +65,7 @@ location_rules: Dict(str, Callable[[CollectionState, int], bool]) = {
             get_kicks(state, player) >= 3,
             state.has("Cling Gem", player),
             can_slidejump(state, player) and get_kicks(state, player) > 0,
-    ),
+            ),
     "Tower - Cling Gem": get_kicks >= 3,
 
     "Bailey - Major Key": lambda state, player:
@@ -65,26 +73,26 @@ location_rules: Dict(str, Callable[[CollectionState, int], bool]) = {
             state.has("Sunsetter", player),
             state.has("Cling Gem", player),
             get_kicks(state, player) >= 3,
-        ),
+    ),
     "Underbelly - Major Key": lambda state, player:
         state.has("Sunsetter", player)
         and any(
-            state.has_all(["Dream Breaker", "Strikebreak", "Soul Cutter"], player) and can_bounce(state, player),
+            can_soulcutter(state, player) and can_bounce(state, player),
             can_slidejump and get_kicks >= 3,
-            state.has_all(["Dream Breaker", "Strikebreak", "Soul Cutter", "Cling Gem"], player),
-        ),
+            can_soulcutter(state, player) and state.has("Cling Gem"),
+    ),
     "Tower - Major Key": lambda state, player:
         state.has("Cling Gem", player) and get_kicks >= 3,
     "Theatre - Major Key": lambda state, player:
-        state.has_all(["Dream Breaker", "Strikebreak", "Soul Cutter"], player)
+        can_soulcutter(state, player)
         and any(
             state.has("Cling Gem", player) and can_slidejump,
             state.has("Cling Gem", player) and get_kicks > 0,
-        ),
+    ),
     "Keep - Major Key": lambda state, player:
         any(
             state.has_all(["Cling Gem", "Sunsetter"], player) and can_bounce(state, player),
             state.has("Cling Gem", player) and can_bounce(state, player) and get_kicks(state, player) >= 3,
             state.has_all(["Sunsetter", "Cling Gem"], player) and can_bounce(state, player) and get_kicks >= 3,
-        )
+    )
 }
