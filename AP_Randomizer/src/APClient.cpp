1e352495b5d37b319a1d86e7d2aefa234bb48d1e
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-09-20 15:57:57 -0500

    Add small_keys int to APClient

--- AP_Randomizer/src/APClient.cpp
+++ AP_Randomizer/src/APClient.cpp
@@ -24,6 +24,7 @@ namespace Pseudoregalia_AP {
     std::map <std::wstring, std::vector<APCollectible>> APClient::zone_table;
     std::map<std::wstring, int> APClient::upgrade_table;
     bool APClient::major_keys[5];
+    int APClient::small_keys;
 
     std::map<std::wstring, int> APClient::GetUpgradeTable() {
         return upgrade_table;
