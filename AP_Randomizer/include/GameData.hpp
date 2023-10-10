932fd6da393d871c6f4efda3388595482fbf9f2a
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-10-09 21:00:13 -0500

    Create ItemType enum and GetItemType function in GameData

--- AP_Randomizer/include/GameData.hpp
+++ AP_Randomizer/include/GameData.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 namespace Pseudoregalia_AP {
+	enum class ItemType {
+		Ability,
+		HealthPiece,
+		SmallKey,
+		MajorKey,
+		Unknown
+	};
+
 	class GameData {
 	public:
 		static int GetHealthPieces();
 		static int GetSmallKeys();
 		static bool* GetMajorKeys();
+		static int ReceiveItem(int64_t);
 	private:
+		static ItemType GetItemType(int64_t);
 		static int health_pieces;
 		static int small_keys;
 		static bool major_keys[5];
--- AP_Randomizer/src/GameData.cpp
+++ AP_Randomizer/src/GameData.cpp
@@ -13,4 +13,21 @@ namespace Pseudoregalia_AP {
     bool* GameData::GetMajorKeys() {
         return major_keys;
     }
+
+    ItemType GameData::GetItemType(int64_t id) {
+        id -= 2365810000;
+        if (1 <= id && id <= 18) {
+            return ItemType::Ability;
+        }
+        if (id == 19) {
+            return ItemType::HealthPiece;
+        }
+        if (id == 20) {
+            return ItemType::SmallKey;
+        }
+        if (21 <= id && id <= 25) {
+            return ItemType::MajorKey;
+        }
+        return ItemType::Unknown;
+    }
 }
