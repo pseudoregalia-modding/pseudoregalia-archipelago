a677131e14b41d54826122a3f26b80327c2d492c
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-10-10 11:52:55 -0500

    Initialize lookup_id_to_upgrade in header file instead of cpp

--- AP_Randomizer/include/GameData.hpp
+++ AP_Randomizer/include/GameData.hpp
@@ -29,7 +29,26 @@ namespace GameData {
 	int health_pieces;
 	int small_keys;
 	bool major_keys[5];
-	const std::map<int64_t, std::wstring> lookup_id_to_upgrade;
 	std::map<std::wstring, int> upgrade_table;
 	std::map<std::wstring, std::vector<APCollectible>> collectible_table;
+	const std::map<int64_t, std::wstring> lookup_id_to_upgrade = {
+		{2365810001, L"attack"},
+		{2365810002, L"powerBoost"},
+		{2365810003, L"airKick"},
+		{2365810004, L"slide"},
+		{2365810005, L"SlideJump"},
+		{2365810006, L"plunge"},
+		{2365810007, L"chargeAttack"},
+		{2365810008, L"wallRide"},
+		{2365810009, L"Light"},
+		{2365810010, L"projectile"},
+		{2365810011, L"extraKick"},
+		{2365810012, L"airRecovery"},
+		{2365810013, L"mobileHeal"},
+		{2365810014, L"magicHaste"},
+		{2365810015, L"healBoost"},
+		{2365810016, L"damageBoost"},
+		{2365810017, L"magicPiece"},
+		{2365810018, L"outfitPro"},
+	};
 }
--- AP_Randomizer/src/GameData.cpp
+++ AP_Randomizer/src/GameData.cpp
@@ -2,27 +2,6 @@
 #include "GameData.hpp"
 
 namespace GameData {
-    const std::map<int64_t, std::wstring> GameData::lookup_id_to_upgrade = {
-        {2365810001, L"attack"},
-        {2365810002, L"powerBoost"},
-        {2365810003, L"airKick"},
-        {2365810004, L"slide"},
-        {2365810005, L"SlideJump"},
-        {2365810006, L"plunge"},
-        {2365810007, L"chargeAttack"},
-        {2365810008, L"wallRide"},
-        {2365810009, L"Light"},
-        {2365810010, L"projectile"},
-        {2365810011, L"extraKick"},
-        {2365810012, L"airRecovery"},
-        {2365810013, L"mobileHeal"},
-        {2365810014, L"magicHaste"},
-        {2365810015, L"healBoost"},
-        {2365810016, L"damageBoost"},
-        {2365810017, L"magicPiece"},
-        {2365810018, L"outfitPro"},
-    };
-
     int GameData::GetHealthPieces() {
         return health_pieces;
     }
