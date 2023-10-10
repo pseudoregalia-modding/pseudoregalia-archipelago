f5e4496485564825183fe3ca98113a5bebfcb7e8
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-10-09 23:01:53 -0500

    Reorder members in GameData header

--- AP_Randomizer/include/GameData.hpp
+++ AP_Randomizer/include/GameData.hpp
@@ -15,21 +15,21 @@ namespace Pseudoregalia_AP {
 
 	class GameData {
 	public:
+		static void Initialize();
 		static int GetHealthPieces();
 		static int GetSmallKeys();
 		static bool* GetMajorKeys();
 		static std::map<std::wstring, int> GetUpgradeTable();
-		static void Initialize();
-		static void ReceiveItem(int64_t);
 		static std::vector<APCollectible> GetCollectiblesOfZone(std::wstring);
 		static void CheckLocation(int64_t);
+		static void ReceiveItem(int64_t);
 	private:
 		static ItemType GetItemType(int64_t);
-		const static std::map<int64_t, std::wstring> lookup_id_to_upgrade;
-		static std::map<std::wstring, std::vector<APCollectible>> collectible_table;
-		static std::map<std::wstring, int> upgrade_table;
 		static int health_pieces;
 		static int small_keys;
 		static bool major_keys[5];
+		const static std::map<int64_t, std::wstring> lookup_id_to_upgrade;
+		static std::map<std::wstring, int> upgrade_table;
+		static std::map<std::wstring, std::vector<APCollectible>> collectible_table;
 	};
 }
