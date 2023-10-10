43bfc06029daec8fa5f5f903a926cb5ca5490914
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-10-09 20:45:04 -0500

    Create variables and getters for health pieces, small keys, and major keys in GameData

--- AP_Randomizer/include/GameData.hpp
+++ AP_Randomizer/include/GameData.hpp
@@ -3,6 +3,12 @@
 namespace Pseudoregalia_AP {
 	class GameData {
 	public:
+		static int GetHealthPieces();
+		static int GetSmallKeys();
+		static bool* GetMajorKeys();
 	private:
+		static int health_pieces;
+		static int small_keys;
+		static bool major_keys[5];
 	};
 }
--- AP_Randomizer/src/GameData.cpp
+++ AP_Randomizer/src/GameData.cpp
@@ -2,5 +2,15 @@
 #include "GameData.hpp"
 
 namespace Pseudoregalia_AP {
+    int GameData::GetHealthPieces() {
+        return health_pieces;
+    }
 
+    int GameData::GetSmallKeys() {
+        return small_keys;
+    }
+
+    bool* GameData::GetMajorKeys() {
+        return major_keys;
+    }
 }
