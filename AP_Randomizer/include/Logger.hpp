c5276de84c792b3d9f4a58fbbf06d9806e372b74
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-10-11 11:11:58 -0500

    Create LogType enum in Logger

--- AP_Randomizer/include/Logger.hpp
+++ AP_Randomizer/include/Logger.hpp
@@ -5,6 +5,14 @@
 namespace Logger {
 	using RC::Unreal::FText;
 
+	enum class LogType {
+		Default,
+		Popup,
+		System,
+		Warning,
+		Error
+	};
+
 	void PrintToPlayer(std::string);
 	void OnTick();
 	void ToggleMessageMute();
