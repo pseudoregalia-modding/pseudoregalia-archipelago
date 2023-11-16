8e0dafed10a4593ff197cc7aa1bc9c92a4a99edc
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-11-16 15:53:56 -0600

    Update comments and new location ids in collectible table

--- AP_Randomizer/src/GameData.cpp
+++ AP_Randomizer/src/GameData.cpp
@@ -192,122 +192,122 @@ namespace GameData {
                 {2365810001, Collectible(FVector(-3500.0, 4950.0, -50.0))},
             // Slide
                 {2365810002, Collectible(FVector(16650, 2600, 2350))},
-            // Good Graces, dark rooms
-                {2365810016, Collectible(FVector(18250, -9750, 4200))},
-            // Small key, Strong Eyes
-                {2365810024, Collectible(FVector(750, 8850, 2650))},
-            // Small key, rafters
-                {2365810023, Collectible(FVector(7487, 1407, 4250))},
-            // HP, alcove by mirror
-                {2365810030, Collectible(FVector(1150, -400, 1050))},
-            // HP, past poles
-                {2365810031, Collectible(FVector(6800, 8850, 3850))},
+            // Alcove Near Mirror
+                {2365810003, Collectible(FVector(1150, -400, 1050))},
+            // Dark Orbs
+                {2365810004, Collectible(FVector(18250, -9750, 4200))},
+            // Past Poles
+                {2365810005, Collectible(FVector(6800, 8850, 3850))},
+            // Rafters
+                {2365810006, Collectible(FVector(7487, 1407, 4250))},
+            // Strong Eyes
+                {2365810007, Collectible(FVector(750, 8850, 2650))},
                     }},
             {Map::Castle, unordered_map<int64_t, Collectible> {
             // Indignation
-                {2365810003, Collectible(FVector(5400, 2100, -550))},
-            // Locked door
-                {2365810022, Collectible(FVector(2700, -1700, -500))},
-            // Pilgrimage, floater in courtyard
-                {2365810013, Collectible(FVector(-5000, -600, 2050))},
-            // Small key, platform in main halls
-                {2365810025, Collectible(FVector(7950, 2750, -200))},
-            // Small key, high climb near wheel crawlers
-                {2365810026, Collectible(FVector(-4100, -8200, 2950))},
-            // Clear Mind, near theatre
-                {2365810017, Collectible(FVector(3390, 21150, 6600))},
-            // Good Graces, high climb from courtyard
-                {2365810019, Collectible(FVector(-3150, 11500, 6300))},
-            // HP, near dungeon
-                {2365810032, Collectible(FVector(1600, 8000, -1400))},
-            // HP, corner corridor
-                {2365810033, Collectible(FVector(11850, 1000, -300))},
-            // HP, wheel Crawlers
-                {2365810034, Collectible(FVector(-10050, -3700, 1000))},
-            // HP, alcove near theatre
-                {2365810035, Collectible(FVector(-9600, 21750, 5400))},
-            // HP, balcony
-                {2365810036, Collectible(FVector(16400, 3800, 1200))},
+                {2365810008, Collectible(FVector(5400, 2100, -550))},
+            // Alcove Near Dungeon
+                {2365810009, Collectible(FVector(1600, 8000, -1400))},
+            // Balcony
+                {2365810010, Collectible(FVector(16400, 3800, 1200))},
+            // Corner Corridor
+                {2365810011, Collectible(FVector(11850, 1000, -300))},
+            // Floater In Courtyard
+                {2365810012, Collectible(FVector(-5000, -600, 2050))},
+            // Locked Door
+                {2365810013, Collectible(FVector(2700, -1700, -500))},
+            // Platform In Main Halls
+                {2365810014, Collectible(FVector(7950, 2750, -200))},
+            // Tall Room Near Wheel Crawlers
+                {2365810015, Collectible(FVector(-4100, -8200, 2950))},
+            // Wheel Crawlers
+                {2365810016, Collectible(FVector(-10050, -3700, 1000))},
+            // High Climb From Courtyard
+                {2365810017, Collectible(FVector(-3150, 11500, 6300))},
+            // Alcove Near Scythe Corridor
+                {2365810018, Collectible(FVector(-9600, 21750, 5400))},
+            // Near Theatre Front
+                {2365810019, Collectible(FVector(3390, 21150, 6600))},
                     }},
             {Map::Keep, unordered_map<int64_t, Collectible> {
-            // Sunsetter
-                {2365810004, Collectible(FVector(-3000, 4900, -400))},
             // Strikebreak
-                {2365810005, Collectible(FVector(10050, 1800, 1000))},
-            // Keep major key
-                {2365810049, Collectible(FVector(14350, -50, 1350))},
-            // Clear Mind near theatre
-                {2365810021, Collectible(FVector(-3900, -6109, -450))},
-            // small key, lever room
-                {2365810027, Collectible(FVector(1050, 15700, 1300))},
-                // HP, alcove near locked door
-                {2365810039, Collectible(FVector(800, 2500, 1200))},
+                {2365810020, Collectible(FVector(10050, 1800, 1000))},
+            // Alcove Near Locked Door
+                {2365810021, Collectible(FVector(800, 2500, 1200))},
+            // Levers Room
+                {2365810022, Collectible(FVector(1050, 15700, 1300))},
+            // Lonely Throne
+                {2365810023, Collectible(FVector(14350, -50, 1350))},
+            // Near Theatre
+                {2365810024, Collectible(FVector(-3900, -6109, -450))},
+            // Sunsetter
+                {2365810025, Collectible(FVector(-3000, 4900, -400))},
                     }},
             {Map::Library, unordered_map<int64_t, Collectible> {
-            // sun greaves
-                {2365810006, Collectible(FVector(-4150, 9200, -100), vector<pair<string, int>>{{"split_sun_greaves", false}})},
-            // split greaves 1
+            // Sun Greaves
+                {2365810026, Collectible(FVector(-4150, 9200, -100), vector<pair<string, int>>{{"split_sun_greaves", false}})},
+            // Upper Back
+                {2365810027, Collectible(FVector(-9250, -1850, 1250))},
+            // Locked Door Across
+                {2365810028, Collectible(FVector(-1300, -6750, -700))},
+            // Locked Door Left
+                {2365810029, Collectible(FVector(-3750, -4170, -700))},
+            // Split Greaves 1
                 {2365810051, Collectible(FVector(-4150, 9160, 0), vector<pair<string, int>>{{"split_sun_greaves", true}})},
-            // split greaves 2
+            // Split Greaves 2
                 {2365810052, Collectible(FVector(-4100, 9250, -100), vector<pair<string, int>>{{"split_sun_greaves", true}})},
-            // split greaves 3
+            // Split Greaves 3
                 {2365810053, Collectible(FVector(-4200, 9250, -100), vector<pair<string, int>>{{"split_sun_greaves", true}})},
-            // Clear mind, locked left
-                {2365810020, Collectible(FVector(-1300, -6750, -700))},
-            // HP upper
-                {2365810037, Collectible(FVector(-9250, -1850, 1250))},
-            // HP locked
-                {2365810038, Collectible(FVector(-3750, -4170, -700))},
                     }},
             {Map::Theatre, unordered_map<int64_t, Collectible> {
-            // soul cutter
-                {2365810007, Collectible(FVector(8500, 7850, -1400))},
-            // theatre major key
-                {2365810050, Collectible(FVector(5200, 1550, 700))},
-            // aerial finesse on ceiling beam
-                {2365810012, Collectible(FVector(-14100, -150, 1950))},
-            // empathy, locked door
-                {2365810015, Collectible(FVector(-1460, -2550, 2240))},
-            // HP, murder goat
-                {2365810044, Collectible(FVector(255, 1150, 50))},
-            // HP, back of auditorium
-                {2365810045, Collectible(FVector(-1600, 1500, 2600))},
+            // Soul Cutter
+                {2365810030, Collectible(FVector(8500, 7850, -1400))},
+            // Back Of Auditorium
+                {2365810031, Collectible(FVector(-1600, 1500, 2600))},
+            // Center Stage
+                {2365810032, Collectible(FVector(5200, 1550, 700))},
+            // Locked Door
+                {2365810033, Collectible(FVector(-1460, -2550, 2240))},
+            // Murderous Goat
+                {2365810034, Collectible(FVector(255, 1150, 50))},
+            // Corner Beam
+                {2365810035, Collectible(FVector(-14100, -150, 1950))},
                     }},
             {Map::Bailey, unordered_map<int64_t, Collectible> {
-            // solar wind
-                {2365810008, Collectible(FVector(-1100, 10850, 150))},
-            // bailey major key
-                {2365810046, Collectible(FVector(-1787, 5236, 650))},
-            // empathy, cheese bell
-                {2365810014, Collectible(FVector(5040, 7150, 2500))},
-            // small key, in a bulding
-                {2365810028, Collectible(FVector(3007, 3457, 300))},
-            // hp, center steeple
-                {2365810040, Collectible(FVector(2350, 7260, 2110))},
+            // Solar Wind
+                {2365810036, Collectible(FVector(-1100, 10850, 150))},
+            // Center Steeple
+                {2365810037, Collectible(FVector(2350, 7260, 2110))},
+            // Cheese Bell
+                {2365810038, Collectible(FVector(5040, 7150, 2500))},
+            // Guarded Hand
+                {2365810039, Collectible(FVector(-1787, 5236, 650))},
+            // Inside Building
+                {2365810040, Collectible(FVector(3007, 3457, 300))},
                     }},
             {Map::Underbelly, unordered_map<int64_t, Collectible> {
-            // ascendant light
-                {2365810009, Collectible(FVector(-5400, 6650, 6750))},
-            // underbelly major key
-                {2365810047, Collectible(FVector(31900, 26250, 3850))},
-            // heliacal power, locked door
-                {2365810011, Collectible(FVector(18896, 7937, 1200))},
-            // martial prowess, strikebreak wall
-                {2365810018, Collectible(FVector(11300, 12700, 3107))},
-            // small key, main room
-                {2365810029, Collectible(FVector(-726, 19782, 3200))},
-            // HP, rafters near keep
-                {2365810041, Collectible(FVector(19600, 17750, 5700))},
-            // HP, building near little guy
-                {2365810042, Collectible(FVector(-4350, 28350, 1850))},
-            // HP, alcove in light path
-                {2365810043, Collectible(FVector(-2550, 12300, 4400))},
+            // Ascendant Light
+                {2365810041, Collectible(FVector(-5400, 6650, 6750))},
+            // Alcove Near Light
+                {2365810042, Collectible(FVector(-2550, 12300, 4400))},
+            // Building Near Little Guy
+                {2365810043, Collectible(FVector(-4350, 28350, 1850))},
+            // Locked Door
+                {2365810044, Collectible(FVector(18896, 7937, 1200))},
+            // Main Room
+                {2365810045, Collectible(FVector(-726, 19782, 3200))},
+            // Rafters Near Keep
+                {2365810046, Collectible(FVector(19600, 17750, 5700))},
+            // Strikebreak Wall
+                {2365810047, Collectible(FVector(11300, 12700, 3107))},
+            // Surrounded By Holes
+                {2365810048, Collectible(FVector(31900, 26250, 3850))},
                     }},
             {Map::Tower, unordered_map<int64_t, Collectible> {
-            // cling gem
-                {2365810010, Collectible(FVector(13350, 5250, 4150))},
-            // tower major key
-                {2365810048, Collectible(FVector(9650, 5250, 7100))},
+            // Cling Gem
+                {2365810049, Collectible(FVector(13350, 5250, 4150))},
+            // Atop The Tower
+                {2365810050, Collectible(FVector(9650, 5250, 7100))},
                     }},
         };
 
