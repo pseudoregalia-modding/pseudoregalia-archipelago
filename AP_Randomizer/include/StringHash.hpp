a74388de2090f58db26913166268e0a755c46768
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-11-21 13:24:37 -0600

    Add hash function for narrow strings to use with json tags

--- AP_Randomizer/include/StringHash.hpp
+++ AP_Randomizer/include/StringHash.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <string>
 
-// wstring hashes to run switch statements on strings.
+// string hashes to run switch statements on strings.
+// I've named them differently instead of using an override since it's easy to use the wrong one by accident.
+
+// std::hash isn't constexpr so we need to implement our own hash function.
+// I just copy/pasted this from https://stackoverflow.com/a/48896410
 namespace StringHash {
-	// std::hash isn't constexpr so we need to implement our own hash function.
-	// I just copy/pasted this from https://stackoverflow.com/a/48896410
 	constexpr size_t HashWstring(const std::wstring& to_hash) {
 		// Require size_t to be 64-bit.
 		static_assert(sizeof(size_t) == 8);
@@ -12,6 +14,19 @@ namespace StringHash {
 		// FNV-1a 64 bit algorithm
 		size_t result = 0xcbf29ce484222325; // FNV offset basis
 		for (wchar_t c : to_hash) {
+			result ^= c;
+			result *= 1099511628211; // FNV prime
+		}
+		return result;
+	}
+
+	constexpr size_t HashNstring(const std::string& to_hash) {
+		// Require size_t to be 64-bit.
+		static_assert(sizeof(size_t) == 8);
+
+		// FNV-1a 64 bit algorithm
+		size_t result = 0xcbf29ce484222325; // FNV offset basis
+		for (char c : to_hash) {
 			result ^= c;
 			result *= 1099511628211; // FNV prime
 		}
