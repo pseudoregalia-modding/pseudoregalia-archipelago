4c86e3261ff418f559b4d45f911d31cf1a9f3d47
magicalgirlrosel <141810840+magicalgirlrosel@users.noreply.github.com>
2023-12-18 17:31:09 -0600

    Remove until I have time to implement this fully

--- AP_Randomizer/include/DeathLinkMessages.hpp
+++ AP_Randomizer/include/DeathLinkMessages.hpp
@@ -19,6 +19,7 @@ namespace DeathLinkMessages {
 			L"Are you sure that jump is possible?",
 			L"Maybe that check isn't so important?", // Garbo
 			L"Maybe you should look for an alternate route.",
+			L"Tip: You can lower the logic level in your yaml settings.",
 		};
 
 		// Messages that you see when you die.
@@ -36,7 +37,6 @@ namespace DeathLinkMessages {
 
 		// TODO: figure out a system to make these work cleanly with flags.
 		const std::vector<std::wstring> high_logic_messages = {
-			L"Tip: You can lower the logic level in your yaml settings.",
 		};
 
 		const int own_messages_max = own_deathlink_messages.size() - 1;
@@ -46,11 +46,14 @@ namespace DeathLinkMessages {
 
 	// Returns a random death message for the player who died.
 	std::wstring RandomOwnDeathlink() {
+		// TODO: potentially add situational death link messages like this
+		/*
 		using std::begin, std::end;
 		std::vector<std::wstring> own_messages(own_deathlink_messages);
 		if (GameData::GetOptions().at("logic_level") > 1) {
 			own_messages.insert(end(own_messages), begin(high_logic_messages), end(high_logic_messages));
 		}
+		*/
 		boost::random::uniform_int_distribution<> dist(0, own_messages_max);
 		return own_deathlink_messages[dist(gen)];
 	}
